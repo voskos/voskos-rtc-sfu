529f369b90f73f51ebccd507703d343ffa22db19
preetamnahak007 <preetamnahak@gmail.com>
2020-11-12 00:29:54 +0530

    Initial server setup

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/voskos/voskos-rtc-sfu
 
 go 1.15
 
-require github.com/nats-io/nats.go v1.10.0 // indirect
+require (
+	github.com/gin-gonic/gin v1.6.3 // indirect
+	github.com/nats-io/nats.go v1.10.0 // indirect
+	github.com/pion/rtcp v1.2.4 // indirect
+	github.com/pion/webrtc/v2 v2.2.26 // indirect
+)
--- main.go
+++ main.go
@@ -3,6 +3,10 @@ package main
 import (
 	"fmt"
 	nats "github.com/nats-io/nats.go"
+
+	"github.com/gin-gonic/gin"
+	//"github.com/pion/rtcp"
+	"github.com/pion/webrtc/v2"
 )
 
 type message struct {
@@ -26,14 +30,32 @@ func main() {
 	ec.BindSendChan("foo", producerCh)
 
 	msg := &message{Id: "Local System 001", Content: "Hello, World!"}
-
 	producerCh <- msg
-
 	producerCh <- msg
 
 	who := <- consumerCh
-
 	fmt.Printf("%+v\n",who)
-
 	fmt.Printf("Connectivity Check completed successfully!!\n")
+
+	//***************************************************************************************************//
+	userIdMediaTrackMap := make(map[string]chan *webrtc.Track)
+	fmt.Println(userIdMediaTrackMap)
+
+	m := webrtc.MediaEngine{}
+	// Setup the codecs you want to use.
+	// Only support VP8(video compression), this makes our proxying code simpler
+	m.RegisterCodec(webrtc.NewRTPVP8Codec(webrtc.DefaultPayloadTypeVP8, 90000))
+	api := webrtc.NewAPI(webrtc.WithMediaEngine(m))
+	fmt.Println(api)
+	//
+
+
+	//Starting gin server
+	router := gin.Default()
+
+	router.POST("/offer", func(c *gin.Context) {
+
+	})
+
+	router.Run(":8080")
 }
