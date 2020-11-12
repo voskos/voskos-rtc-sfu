0aaef99dbfa35176bf99c46c2c9336e25a3b3b6d
preetamnahak007 <preetamnahak@gmail.com>
2020-11-13 00:00:15 +0530

    SDP exchange performed through go channels. Will be upgrade to NATS next

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"time"
 	nats "github.com/nats-io/nats.go"
 
-	"github.com/gin-gonic/gin"
 	//"github.com/pion/rtcp"
-	"github.com/pion/webrtc/v2"
+	"github.com/pion/webrtc/v3"
+	"github.com/voskos/voskos-rtc-sfu/signal"
 )
 
 type message struct {
@@ -15,8 +17,86 @@ type message struct {
 }
 
 const NATS_HOST_ = "nats://ec2-3-7-252-87.ap-south-1.compute.amazonaws.com:4222"
+const rtcpPLIInterval = time.Second * 3
+
 
 func main() {
+	//****************************************SDP SERVER - BEGIN *************************************//
+
+	// channels for send/recv SDPs
+	sdpInChan, sdpOutChan := signal.HTTPSDPServer()
+
+	offer := webrtc.SessionDescription{}
+	signal.Decode(<-sdpInChan, &offer)
+	log.Println("OFFER\n", offer) // json format
+
+	// Since we are answering use PayloadTypes declared by offerer
+	mediaEngine := webrtc.MediaEngine{}
+	err := mediaEngine.PopulateFromSDP(offer)
+	if err != nil {
+		panic(err)
+	}
+
+	// Create the API object with the MediaEngine
+	api := webrtc.NewAPI(webrtc.WithMediaEngine(mediaEngine))
+
+	peerConnectionConfig := webrtc.Configuration{
+		ICEServers: []webrtc.ICEServer{
+			{
+				URLs: []string{"stun:stun.l.google.com:19302"},
+			},
+		},
+	}
+
+	// Create a new RTCPeerConnection
+	peerConnection, err := api.NewPeerConnection(peerConnectionConfig)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// Allow us to receive 1 video track
+	if _, err = peerConnection.AddTransceiverFromKind(webrtc.RTPCodecTypeVideo); err != nil {
+		log.Fatalln(err)
+	}
+
+	peerConnection.OnTrack(func(remoteTrack *webrtc.Track, receiver *webrtc.RTPReceiver) {
+		log.Printf("Track recived of kind %s", remoteTrack.Kind())
+		peerConnection.AddTrack(remoteTrack)
+
+	})
+
+	// Set the remote SessionDescription
+	err = peerConnection.SetRemoteDescription(offer)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// Create answer
+	answer, err := peerConnection.CreateAnswer(nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// Create channel that is blocked until ICE Gathering is complete
+	// gatherComplete := webrtc.GatheringCompletePromise(peerConnection)
+	// <-gatherComplete
+
+	// Sets the LocalDescription, and starts our UDP listeners
+	err = peerConnection.SetLocalDescription(answer)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Println("ANSWER for Caster => \n", answer) // json format of SDP Answer
+	// Get the LocalDescription and take it to base64 so we can paste in browser
+	baseAnswer := signal.Encode(answer)
+	sdpOutChan <- baseAnswer
+	//log.Println(baseAnswer)
+
+	//****************************************SDP SERVER - END *************************************//
+
+
+	//****************************************NATS SERVER - BEGIN ************************************//
 	fmt.Printf("Connectivity checking with Nats server !!!\n")
 
 	nc, _ := nats.Connect(NATS_HOST_)
@@ -37,25 +117,10 @@ func main() {
 	fmt.Printf("%+v\n",who)
 	fmt.Printf("Connectivity Check completed successfully!!\n")
 
-	//***************************************************************************************************//
-	userIdMediaTrackMap := make(map[string]chan *webrtc.Track)
-	fmt.Println(userIdMediaTrackMap)
+	//****************************************NATS SERVER - END  ************************************//
+	for{
 
-	m := webrtc.MediaEngine{}
-	// Setup the codecs you want to use.
-	// Only support VP8(video compression), this makes our proxying code simpler
-	m.RegisterCodec(webrtc.NewRTPVP8Codec(webrtc.DefaultPayloadTypeVP8, 90000))
-	api := webrtc.NewAPI(webrtc.WithMediaEngine(m))
-	fmt.Println(api)
-	//
+	}
 
 
-	//Starting gin server
-	router := gin.Default()
-
-	router.POST("/offer", func(c *gin.Context) {
-
-	})
-
-	router.Run(":8080")
 }
